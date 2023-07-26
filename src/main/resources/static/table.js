03258a7db28c410f869157ea801bb3bb086cfc7f
Salmaan Saeed <salsaeed999@gmail.com>
2023-07-26 01:36:24 -0400

    added a button that deletes an employee from the database

---
+++ src/main/resources/static/table.js
@@ -0,0 +1,31 @@
+async function deleteEmployee(id) {
+    console.log("Delete");
+    // curl -X DELETE localhost:8080/employee/delete -d id=53
+
+    // http://localhost:8080/spring-mvc-basics/api/foos?id=abc
+
+    const url = "/employee/delete?id=" + id;
+
+    const delete_confirm = confirm("Are you sure you want to delete this employee?")
+
+    if (delete_confirm === true) {
+        const response = await fetch(url, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            },
+        }).then(async (response) => {
+            if (response.status === 400) {
+                const response_body = await response.json()
+                console.log("Error: " + response_body.error);
+                console.log("Message: " + response_body.message);
+            }
+            else {
+                console.log(response.json())
+                location.reload();
+            }
+        }).catch((error) => {
+            console.log(error);
+        });
+    }
+}
