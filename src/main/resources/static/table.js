3faf5c452cd776a48d4e309b8a8852402f828d52
Salmaan Saeed <salsaeed999@gmail.com>
2023-07-26 16:31:52 -0400

    used axios for the delete employee function

--- src/main/resources/static/table.js
+++ src/main/resources/static/table.js
@@ -2,30 +2,22 @@ async function deleteEmployee(id) {
     console.log("Delete");
     // curl -X DELETE localhost:8080/employee/delete -d id=53
 
-    // http://localhost:8080/spring-mvc-basics/api/foos?id=abc
-
-    const url = "/employee/delete?id=" + id;
-
     const delete_confirm = confirm("Are you sure you want to delete this employee?")
 
     if (delete_confirm === true) {
-        const response = await fetch(url, {
-            method: "DELETE",
-            headers: {
-                "Content-Type": "application/json"
-            },
-        }).then(async (response) => {
-            if (response.status === 400) {
-                const response_body = await response.json()
-                console.log("Error: " + response_body.error);
-                console.log("Message: " + response_body.message);
-            } else {
-                console.log(response.json())
-                location.reload();
-            }
-        }).catch((error) => {
-            console.log(error);
-        });
+        axios.delete("/employee/delete?id=" + id)
+            .then(function (response) {
+                if (response.status === 400) {
+                    console.log("Error: " + response.data.error);
+                    console.log("Message: " + response.data.message);
+                } else {
+                    console.log(response);
+                    location.reload();
+                }
+            })
+            .catch(function (error) {
+                console.log(error)
+            })
     }
 }
 
