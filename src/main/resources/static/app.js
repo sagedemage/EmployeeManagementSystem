bf90519fe169847b792e79a89a0de8475e4bcc1f
Salmaan Saeed <salsaeed999@gmail.com>
2023-08-05 20:48:25 -0400

    added the ui components to the front-end app

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,6 +1,12 @@
 import {ref, computed} from 'vue'
+import Navbar from './components/ui/navbar.js'
+import Footer from './components/ui/footer.js'
 
 export default {
+    components: {
+        Navbar,
+        Footer
+    },
     setup() {
         const count = ref(0)
 
@@ -8,6 +14,8 @@ export default {
     },
     template: /*html*/`
         <div id="body">
+            <Navbar />
+            <Footer />
         </div>
     `
 }
