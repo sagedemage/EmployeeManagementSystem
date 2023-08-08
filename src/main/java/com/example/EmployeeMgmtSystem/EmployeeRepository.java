8cb1058aa453e8e4c199b51470585c17c885e983
Salmaan Saeed <salsaeed999@gmail.com>
2023-08-08 14:16:02 -0400

    minor comment format

--- src/main/java/com/example/EmployeeMgmtSystem/EmployeeRepository.java
+++ src/main/java/com/example/EmployeeMgmtSystem/EmployeeRepository.java
@@ -7,6 +7,6 @@ package com.example.EmployeeMgmtSystem;
 import org.springframework.data.repository.CrudRepository;
 
 /* This will be AUTO IMPLEMENTED by Spring into a Bean called employeeRepository
-   CRUD (Create, Read, Update, Delete) */
+CRUD (Create, Read, Update, Delete) */
 
 public interface EmployeeRepository extends CrudRepository<Employee, Integer> {}
