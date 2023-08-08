46aab3cbfbc47575d14d2c00b9ab77466b84db4b
Salmaan Saeed <salsaeed999@gmail.com>
2023-08-08 02:32:48 -0400

    added unit test for adding an employee

--- src/test/java/com/example/EmployeeMgmtSystem/EmployeeControllerTest.java
+++ src/test/java/com/example/EmployeeMgmtSystem/EmployeeControllerTest.java
@@ -5,15 +5,18 @@ package com.example.EmployeeMgmtSystem;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -27,5 +30,23 @@ public class EmployeeControllerTest {
         this.mvc.perform(get("/employee/fetch?id=1"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("\"id\":1")));
+    }
+
+    @Test
+    public void addEmployee() throws Exception {
+        Object obj = new Object() {
+            public final String name = "john";
+            public final String email = "john@email.com";
+            public final String phone_number = "1112223333";
+        };
+
+        ObjectMapper obj_mapper = new ObjectMapper();
+        String json = obj_mapper.writeValueAsString(obj);
+
+        this.mvc.perform(post("/employee/add")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("\"status_type\":\"Success\"")));
     }
 }
